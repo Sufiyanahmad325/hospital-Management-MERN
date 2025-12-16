56761d6002bcc27d60c2767dea193524f3872d45
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2025-12-16 11:33:06 +0530

    oppointment schma has been created

---
+++ src/models/appointmentSchema.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const appointmentSchema = new mongoose.Schema(
+  {
+    patientId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Patient",
+      required: true
+    },
+
+    doctorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Doctor",
+      required: true
+    },
+
+    date: { type: String, required: true }, // "2025-12-12"
+
+    timeSlot: { type: String, required: true }, // "10:00 AM"
+
+    status: {
+      type: String,
+      enum: ["pending", "cancelled", "completed"],
+      default: "pending"
+    }
+  },
+  { timestamps: true }
+);
+
+export const Appointment = mongoose.model('Appointment' , appointmentSchema)
