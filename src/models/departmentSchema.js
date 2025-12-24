3f574fe1343145a8032154a8a040dd05e6c8ea74
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2025-12-24 11:31:07 +0530

    fixed bugs

--- src/models/departmentSchema.js
+++ src/models/departmentSchema.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const departmentSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true, unique: true, trim: true },
+    nameOfDepartment: { type: String, required: true, unique: true, trim: true },
     description: { type: String, default: "" },
   },
   { timestamps: true }
