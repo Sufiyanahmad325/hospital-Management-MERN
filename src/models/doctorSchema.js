7422bb2ac40a266fb541ef334286d5ed4a3c347b
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2025-12-24 11:29:54 +0530

    fix some bugs

--- src/models/doctorSchema.js
+++ src/models/doctorSchema.js
@@ -2,20 +2,61 @@ import mongoose from "mongoose";
 
 const doctorSchema = new mongoose.Schema(
   {
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
 
-    department: { type: mongoose.Schema.Types.ObjectId, ref: "Department", required: true },
+    department: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Department",
+      required: true,
+    },
 
-    experience: { type: Number, required: true },
+    experience: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
 
-    // availableDays: [{ type: String, required: true }], // ["Mon", "Tue", "Fri"]
+    specialization: {
+      type: String,
+      required: true,
+      enum: [
+        // ❤️ Heart
+        "Interventional Cardiology",
+        "Pediatric Cardiology",
+        "Electrophysiology",
+        "Heart Failure Specialist",
+        "Preventive Cardiology",
 
-    // availableSlots: [{ type: String, required: true }] // ["10:00 AM", "11:00 AM"]
-    availableDays: ["Mon", "Tue", "Fri"],
-    availableSlots: ["10:00 AM", "11:00 AM"]
+        // 🧠 Brain
+        "Neurologist",
+        "Neurosurgeon",
+        "Neurocritical Care Specialist",
 
+        // 🍽️ Stomach
+        "Gastroenterologist",
+        "Hepatologist",
+        "Gastrointestinal Surgeon",
+      ],
+    },
+
+    availableDays: {
+      type: [String],
+      enum: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+      default: [],
+    },
+
+    availableSlots: {
+      type: [String],
+      enum: ['11:00 AM', '11:15 AM', '11:30 AM', '11:45 AM', '12:00 PM', '12:15 PM', '12:30 PM', '12:45 PM', '01:00 PM', '01:15 PM', '01:30 PM', '01:45 PM', '02:00 PM', '02:15 PM', '02:30 PM', '02:45 PM', '03:00 PM', '03:15 PM', '03:30 PM', '03:45 PM', '04:00 PM'],
+      default: [],
+      required: true,
+    },
   },
-  { timestamps: true }
+  { timestamps: true } 
 );
 
-export const Doctor = mongoose.model('Doctor', doctorSchema)
+export const Doctor = mongoose.model("Doctor", doctorSchema);
