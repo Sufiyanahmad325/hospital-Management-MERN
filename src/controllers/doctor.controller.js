cedc845f908793e2c93debc972b775f9dfaca78c
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2025-12-26 08:03:09 +0530

    update Appointment for doctor has been completed

--- src/controllers/doctor.controller.js
+++ src/controllers/doctor.controller.js
@@ -53,4 +53,68 @@ export const getDoctorAppointments = async (req, res) => {
 
 
 
+export const updateAppointmentStatus = async (req, res) => {
+  try {
+    const appointmentId = req.params.id;
+    const { status } = req.body; // completed | cancelled
+    const userId = req.user.id;
 
+    console.log( 'appointmentId :', appointmentId , 'status: ' ,status , 'userId : ' , userId )
+
+    if (!["completed", "cancelled"].includes(status)) {
+      return res
+        .status(400)
+        .json(new ApiResponse(400, null, "Invalid status value"));
+    }
+
+    // 1️⃣ Find doctor
+    const doctor = await Doctor.findOne({ user_id: userId });
+    if (!doctor) {
+      return res
+        .status(404)
+        .json(new ApiResponse(404, null, "Doctor profile not found"));
+    }
+
+    // 2️⃣ Find appointment
+    const appointment = await Appointment.findOne({
+      _id: appointmentId,
+      doctorId: doctor._id,
+    });
+
+    if (!appointment) {
+      return res
+        .status(404)
+        .json(new ApiResponse(404, null, "Appointment not found"));
+    }
+
+    if (appointment.status !== "pending") {
+      return res
+        .status(400)
+        .json(
+          new ApiResponse(
+            400,
+            null,
+            "Only pending appointments can be updated"
+          )
+        );
+    }
+
+    // 3️⃣ Update status
+    appointment.status = status;
+await appointment.save();
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          appointment,
+          `Appointment ${status} successfully`
+        )
+      );
+  } catch (error) {
+    return res
+      .status(500)
+      .json(new ApiResponse(500, null, error.message));
+  }
+};
