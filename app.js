814202120e0471a93a6994bbf855745720623d57
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2025-12-28 12:18:20 +0530

    fixed some error

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ import cookieParser from 'cookie-parser';
 import express from 'express';
 import cors from 'cors';
 
+import authRoutes from "./src/routes/auth.routes.js";
+import adminRoutes from "./src/routes/admin.routes.js";
+import doctorRoutes from "./src/routes/doctor.routes.js";
+import patientRoutes from "./src/routes/patient.routes.js";
+import appointmentRoutes from "./src/routes/appointment.routes.js";
+
 const app = express();
 
 
@@ -12,6 +18,16 @@ app.use(express.static('public')); // it is used to serve static files from publ
 app.use(cookieParser()); // it is used to parse cookies
 
 app.use(cors({origin: process.env.CORS_ORIGIN || "http://localhost:3000" , credentials: true})); // it is used to enable cors such as accessing api from different domain , creadentials: it is used to send cookies from client to server
+
+
+
+app.use("/hospital/auth", authRoutes);
+app.use("/hospital/admin", adminRoutes);
+app.use("/hospital/doctor", doctorRoutes);
+app.use("/hospital/patient", patientRoutes);
+app.use("/hospital/appointments", appointmentRoutes);
+
+
 
 
 app.get('/', (req ,res)=>{
