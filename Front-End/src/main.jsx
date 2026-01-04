653c325e34af9488d23d79806a5d8915f7dc9baa
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2026-01-04 08:20:14 +0530

    react cookie has been addded

--- Front-End/src/main.jsx
+++ Front-End/src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { store } from './reduxtollkit/store.js'
+import { Provider } from 'react-redux'
+import { CookiesProvider } from 'react-cookie'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <CookiesProvider>
+  <Provider store={store}>
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  </Provider>
+  </CookiesProvider>
 )
