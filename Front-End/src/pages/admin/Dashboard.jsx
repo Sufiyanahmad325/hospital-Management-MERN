19fdb2043ee4b46edbe5ab28e5958195787c3bd8
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2026-01-06 14:44:41 +0530

    dash board has been connected with backend

--- Front-End/src/pages/admin/Dashboard.jsx
+++ Front-End/src/pages/admin/Dashboard.jsx
@@ -1,9 +1,37 @@
+import { useCookies } from "react-cookie";
 import { FaUsers, FaCalendarCheck } from "react-icons/fa";
 import { FaUserDoctor } from "react-icons/fa6";
 import { MdLocalHospital } from "react-icons/md";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllDepartments, getAllDoctorAppointments, getAllDoctors, getAllPatients, todayAllAppointments } from "../../reduxtollkit/hospitalManagementSlice";
+import { useEffect } from "react";
 
 
 const Dashboard = () => {
+  const [cookies, setCookie, removeCookie] = useCookies(['accessToken']);
+  const dispatch = useDispatch();
+
+
+  const { totalDoctors, todayPendingCompletedAppointments, totalDoctorsAppointments, totalPatients, totalDepartments, } = useSelector((state) => state.hospitalManagement);
+
+
+  console.log('all total totalDoctorsAppointments====>', totalDoctorsAppointments)
+
+
+
+  useEffect(() => {
+    if (cookies.accessToken) {
+      // Fetch dashboard data here
+      dispatch(getAllDoctors());
+      dispatch(todayAllAppointments());
+      dispatch(getAllDoctorAppointments());
+      dispatch(getAllPatients());
+      dispatch(getAllDepartments());
+
+    }
+  }, [cookies.accessToken, dispatch]);
+
+
   return (
     <div className="w-[100vw] sm:space-y-6 sm:w-[75vw] p-6 bg-blue-400 sm:h-screen overflow-y-auto">
       {/* Page Title */}
@@ -13,13 +41,13 @@ const Dashboard = () => {
 
       {/* ================= STATS CARDS ================= */}
       <div className="grid grid-cols-1 sm:flex-wrap sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 ">
-        
+
         {/* Total Appointments */}
         <div className="bg-white p-5 rounded-xl shadow flex items-center gap-4">
           <FaCalendarCheck className="text-4xl text-blue-600" />
           <div>
             <p className="text-gray-500 text-sm">Today Appointments</p>
-            <h2 className="text-2xl font-bold">72</h2>
+            <h2 className="text-2xl font-bold">{todayPendingCompletedAppointments.length || '00'}</h2>
           </div>
         </div>
 
@@ -27,7 +55,7 @@ const Dashboard = () => {
           <FaCalendarCheck className="text-4xl text-blue-600" />
           <div>
             <p className="text-gray-500 text-sm">Total Appointments</p>
-            <h2 className="text-2xl font-bold">72</h2>
+            <h2 className="text-2xl font-bold">{totalDoctorsAppointments.length || '00'}</h2>
           </div>
         </div>
 
@@ -36,7 +64,7 @@ const Dashboard = () => {
           <FaUserDoctor className="text-4xl text-green-600" />
           <div>
             <p className="text-gray-500 text-sm">Total Doctors</p>
-            <h2 className="text-2xl font-bold">21</h2>
+            <h2 className="text-2xl font-bold">{totalDoctors.length || '00'}</h2>
           </div>
         </div>
 
@@ -45,7 +73,7 @@ const Dashboard = () => {
           <FaUsers className="text-4xl text-purple-600" />
           <div>
             <p className="text-gray-500 text-sm">Total Patients</p>
-            <h2 className="text-2xl font-bold">80</h2>
+            <h2 className="text-2xl font-bold">{totalPatients.length || '00'}</h2>
           </div>
         </div>
 
@@ -54,7 +82,7 @@ const Dashboard = () => {
           <MdLocalHospital className="text-4xl text-red-600" />
           <div>
             <p className="text-gray-500 text-sm">Departments</p>
-            <h2 className="text-2xl font-bold">8</h2>
+            <h2 className="text-2xl font-bold">{totalDepartments.length || '00'}</h2>
           </div>
         </div>
       </div>
@@ -77,37 +105,31 @@ const Dashboard = () => {
             </thead>
 
             <tbody>
-              <tr className="border-b">
-                <td className="p-3">Rahul Kumar</td>
-                <td className="p-3">Dr. Smith</td>
-                <td className="p-3">12 Aug 2025</td>
-                <td className="p-3 text-green-600 font-semibold">
-                  Completed
-                </td>
-              </tr>
 
-              <tr className="border-b">
-                <td className="p-3">Amit Sharma</td>
-                <td className="p-3">Dr. John</td>
-                <td className="p-3">13 Aug 2025</td>
-                <td className="p-3 text-orange-500 font-semibold">
-                  Pending
-                </td>
-              </tr>
+              {
+                totalDoctorsAppointments?.length > 0 ? (
+                  totalDoctorsAppointments?.map((ele, ind) => (
+                    <tr key={ind} className="border-b">
+                      <td className="p-3">{ele.patientId.name}</td>
+                      <td className="p-3">{ele.doctorId.user_id.name}</td>
+                      <td className="p-3">{ele.date}</td>
+                      <td className={`p-3 font-bold
+                        ${ele.status === "pending" ? 'text-yellow-400' : ele.status == 'completed' ? 'text-green-500' : 'text-red-600 font-semibold'}`}>
+                      {ele.status}
+                    </td>
+                    </tr>
+            ))
+            ) :
+            (
+            <div>Please login your Account</div>
+            )
+              }
 
-              <tr>
-                <td className="p-3">Neha Singh</td>
-                <td className="p-3">Dr. Watson</td>
-                <td className="p-3">14 Aug 2025</td>
-                <td className="p-3 text-red-500 font-semibold">
-                  Cancelled
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+          </tbody>
+        </table>
       </div>
     </div>
+    </div >
   );
 };
 
