3b1390c623e6dd74bfd23eb60dbb6f18954c010c
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2026-01-10 21:33:12 +0530

    department has been create successfully created with edited feature

--- Front-End/src/pages/admin/Departments.jsx
+++ Front-End/src/pages/admin/Departments.jsx
@@ -1,6 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { addDepartment, editDepartment, getAllDepartments, setEditDepartment } from '../../reduxtollkit/hospitalManagementSlice';
 
 const Departments = () => {
+  const [nameOfDepartment, setNameOfDepartment] = useState('')
+  const [description, setDescription] = useState('')
+
+  const dispatch = useDispatch()
+  const { totalDepartments, isEditDepartment } = useSelector((state) => state.hospitalManagement)
+
+  const handleAddDepartments = async () => {
+    if (isEditDepartment != null) {
+      let response = await dispatch(editDepartment({ id: isEditDepartment._id, nameOfDepartment: nameOfDepartment, description: description })).unwrap()
+      if (response.success) {
+        setNameOfDepartment('')
+        setDescription('')
+        dispatch(getAllDepartments())
+      }
+      return
+    }
+    let details = { nameOfDepartment, description }
+    let a = await dispatch(addDepartment(details)).unwrap()
+    dispatch(getAllDepartments())
+  }
+
+  const handleEditDepartment = (ele) => {
+    dispatch(setEditDepartment(ele))
+  }
+
+  
+
+  useEffect(() => {
+    if (isEditDepartment != null) {
+      setNameOfDepartment(isEditDepartment.nameOfDepartment)
+      setDescription(isEditDepartment.description)
+    }
+  }, [isEditDepartment])
+
+
   return (
     <div className="min-h-screen bg-blue-400 p-6 space-y-6 sm:w-[75vw] sm:overflow-y-auto sm:h-screen">
 
@@ -9,7 +46,7 @@ const Departments = () => {
         Department Management
       </h1>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+      <div className="grid grid-cols-1 sm:flex sm:flex-col gap-3">
 
         {/* ================= ADD DEPARTMENT ================= */}
         <div className="bg-white p-6 rounded-lg shadow">
@@ -17,28 +54,34 @@ const Departments = () => {
             Add New Department
           </h2>
 
-          <div className="space-y-4">
+          <div className="space-y-4 ">
             <div>
-              <label className="block mb-1 font-medium">
+              <label className=" mb-1 font-medium">
                 Department Name
               </label>
               <input
+                value={nameOfDepartment}
+                onChange={(e) => setNameOfDepartment(e.target.value)}
                 placeholder="Enter department name"
                 className="border p-2 rounded w-full"
               />
             </div>
 
             <div>
-              <label className="block mb-1 font-medium">
+              <label className=" mb-1 font-medium">
                 Description
               </label>
               <textarea
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
                 placeholder="Enter department description"
                 className="border p-2 rounded w-full"
               />
             </div>
 
-            <button className="bg-blue-500 text-white px-4 py-2 rounded">
+            <button
+              onClick={() => handleAddDepartments()}
+              className="bg-blue-500 text-white px-4 py-2 rounded">
               Add Department
             </button>
           </div>
@@ -46,7 +89,7 @@ const Departments = () => {
 
         {/* ================= ALL DEPARTMENTS ================= */}
         <div className="bg-white p-6 rounded-lg shadow">
-          <h2 className="text-lg font-semibold text-green-600 mb-4">
+          <h2 className="text-lg font-semibold text-blue-600 mb-4">
             All Departments
           </h2>
 
@@ -62,53 +105,25 @@ const Departments = () => {
               </thead>
 
               <tbody>
-                <tr className="hover:bg-gray-50">
-                  <td className="border p-2">Cardiology</td>
-                  <td className="border p-2">
-                    Heart related treatments
-                  </td>
-                  <td className="border p-2 text-center">
-                    <button className="bg-red-500 text-white px-3 py-1 rounded">
-                      Delete
-                    </button>
-                  </td>
-                </tr>
+                {
+                  Array.isArray(totalDepartments) && totalDepartments.map((ele, ind) => (
+                    <tr className="hover:bg-gray-50" key={ind}>
+                      <td className="border p-2">{ele.nameOfDepartment}</td>
+                      <td className="border p-2">
+                        {ele.description}
+                      </td>
+                      <td className="border p-2 text-center ">
+                        <button
+                          onClick={() => { handleEditDepartment(ele) }}
+                          className="bg-blue-300 text-black px-3 py-1 rounded hover:bg-blue-400 sm:px-6">
+                          Edit
+                        </button>
+                      </td>
+                    </tr>
+                  ))
+                }
 
-                <tr className="hover:bg-gray-50">
-                  <td className="border p-2">Neurology</td>
-                  <td className="border p-2">
-                    Brain and nervous system
-                  </td>
-                  <td className="border p-2 text-center">
-                    <button className="bg-red-500 text-white px-3 py-1 rounded">
-                      Delete
-                    </button>
-                  </td>
-                </tr>
 
-                <tr className="hover:bg-gray-50">
-                  <td className="border p-2">Orthopedics</td>
-                  <td className="border p-2">
-                    Bone and joint care
-                  </td>
-                  <td className="border p-2 text-center">
-                    <button className="bg-red-500 text-white px-3 py-1 rounded">
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-
-                <tr className="hover:bg-gray-50">
-                  <td className="border p-2">Pediatrics</td>
-                  <td className="border p-2">
-                    Child healthcare
-                  </td>
-                  <td className="border p-2 text-center">
-                    <button className="bg-red-500 text-white px-3 py-1 rounded">
-                      Delete
-                    </button>
-                  </td>
-                </tr>
               </tbody>
 
             </table>
