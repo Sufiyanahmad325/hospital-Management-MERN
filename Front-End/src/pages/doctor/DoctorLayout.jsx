0c177e2f1b30e031e5911de6c835f9d1c899921f
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2026-01-13 21:41:33 +0530

    calling function here

--- Front-End/src/pages/doctor/DoctorLayout.jsx
+++ Front-End/src/pages/doctor/DoctorLayout.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import DoctorSidebar from './doctorSidebar'
 import { Outlet } from 'react-router-dom'
+import { useDispatch } from 'react-redux';
+import { getAllTodayAppointments, getTodayAllPendingAppointment } from '../../reduxtollkit/doctorControlSlice';
 
 function DoctorLayout() {
+  const dispatch = useDispatch()
+  
+  useEffect(()=>{
+    dispatch(getAllTodayAppointments())
+    dispatch(getTodayAllPendingAppointment())
+  },[])
   return (
     <div className='flex flex-col sm:flex-row'>
       <DoctorSidebar />
