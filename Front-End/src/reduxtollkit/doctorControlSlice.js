5bcd32736ba3d03cdaad581fcfbd0a3521f8d166
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2026-01-13 21:36:58 +0530

    doctor toolkit has been create

---
+++ Front-End/src/reduxtollkit/doctorControlSlice.js
@@ -0,0 +1,62 @@
+import { createAsyncThunk, createSlice, isRejectedWithValue } from "@reduxjs/toolkit"
+import axios from "axios"
+
+export const getAllTodayAppointments = createAsyncThunk(
+    'doctorControl/getAllTodayAppointments',
+    async (_, { rejectWithValue }) => {
+        try {
+            let response = await axios.get("http://localhost:8000/hospital/doctor/getTodayappointments",
+                { withCredentials: true }
+            )
+            return response.data.data
+        } catch (error) {
+            rejectWithValue(error.data?.data?.message || 'something went wronge')
+        }
+    }
+)
+
+
+
+
+const initialState = {
+    doctorTodayAllAppointments: [],
+    doctorTodayPendingAppointments: [],
+    doctorTodayCompleteAppointment: [],
+    doctorAllDayTotalAppointments: [],
+    isLoading: false
+}
+
+
+
+
+const doctorControlSlice = createSlice({
+    name: 'doctorControl',
+    initialState,
+    reducers: {
+
+    },
+    extraReducers: (builder) => {
+        builder
+            // getAllTodayAppointments by the Doctor
+            .addCase(getAllTodayAppointments.pending, (state, action) => {
+                state.isLoading = true
+            })
+            .addCase(getAllTodayAppointments.fulfilled, (state, action) => {
+                state.doctorTodayAllAppointments = action.payload
+            })
+            .addCase(getAllTodayAppointments.rejected, (state, action) => {
+                state.isLoading = false
+                state.error = action.error.message
+            })
+
+            //get all today pending appointment
+
+
+
+
+    }
+
+})
+
+
+export const doctorControlReducer = doctorControlSlice.reducer 
