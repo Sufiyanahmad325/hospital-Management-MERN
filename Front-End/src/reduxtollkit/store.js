d5cb0f3b3cfe9a58bb12f9a180f661164a39e899
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2026-01-13 21:37:30 +0530

    store has been updated

--- Front-End/src/reduxtollkit/store.js
+++ Front-End/src/reduxtollkit/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { hospitalManagementSliceReducer } from './hospitalManagementSlice';
+import { doctorControlReducer } from './doctorControlSlice';
+
 
 export const store = configureStore({
   reducer: {
     hospitalManagement: hospitalManagementSliceReducer,
+    doctorControl:doctorControlReducer,
   },
 });
 
