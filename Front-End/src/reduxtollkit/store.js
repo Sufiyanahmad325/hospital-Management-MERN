e5d1e91fa1d356eea2f8989457e9ce0e1aaceec8
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2026-01-17 08:39:00 +0530

    ""

--- Front-End/src/reduxtollkit/store.js
+++ Front-End/src/reduxtollkit/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { hospitalManagementSliceReducer } from './hospitalManagementSlice';
 import { doctorControlReducer } from './doctorControlSlice';
+import { patientControlReducer } from './patientControlSlice';
 
 
 export const store = configureStore({
   reducer: {
     hospitalManagement: hospitalManagementSliceReducer,
     doctorControl:doctorControlReducer,
+    patientControl:patientControlReducer
   },
 });
 
