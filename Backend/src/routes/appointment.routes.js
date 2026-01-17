e6d9aaea7ff42f65385f81cc11316248f8777761
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2026-01-17 08:42:15 +0530

    added some routes is here

--- Backend/src/routes/appointment.routes.js
+++ Backend/src/routes/appointment.routes.js
@@ -3,7 +3,7 @@ import express from "express";
 
 import { verifyJWT } from "../middlewares/verifyJWT.middleware.js";
 import { isPatient } from "../middlewares/role.middleware.js";
-import { bookAppointment, cancelAppointment, getMyAppointments } from "../controllers/Appointment.Controller.js";
+import { bookAppointment, cancelAppointment, getCompleteAppointment, getMyAppointments, getUpComingAppointment } from "../controllers/Appointment.Controller.js";
 
 const router = express.Router();
 
@@ -11,6 +11,8 @@ router.use(verifyJWT, isPatient);
 
 router.post("/bookAppointment", bookAppointment);
 router.get("/getMyAppointments", getMyAppointments);
+router.get('/getUpComingAppointment' , getUpComingAppointment)
+router.get('/getCompleteAppointment' , getCompleteAppointment)
 router.put("/:appointmentId/cancelAppointment", cancelAppointment);
 
 export default router;
