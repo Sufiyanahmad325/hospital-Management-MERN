049d58b7b5628f2378ab74805150e1fc5eed3928
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2026-01-17 08:40:50 +0530

    added some calling async thanks

--- Front-End/src/pages/doctor/DoctorLayout.jsx
+++ Front-End/src/pages/doctor/DoctorLayout.jsx
@@ -2,19 +2,23 @@ import React, { useEffect } from 'react'
 import DoctorSidebar from './doctorSidebar'
 import { Outlet } from 'react-router-dom'
 import { useDispatch } from 'react-redux';
-import { getAllTodayAppointments, getTodayAllPendingAppointment } from '../../reduxtollkit/doctorControlSlice';
+import { getAllDayAppointment, getAllTodayAppointments, getDoctorDetails, getTodayAllPendingAppointment } from '../../reduxtollkit/doctorControlSlice';
 
 function DoctorLayout() {
   const dispatch = useDispatch()
-  
-  useEffect(()=>{
+
+  useEffect(() => {
     dispatch(getAllTodayAppointments())
     dispatch(getTodayAllPendingAppointment())
-  },[])
+    dispatch(getAllDayAppointment())
+    dispatch(getDoctorDetails())
+  }, [])
   return (
     <div className='flex flex-col sm:flex-row'>
       <DoctorSidebar />
+      <div className='sm:max-h-[100vh] sm:overflow-y-auto'>
       <Outlet />
+      </div>
     </div>
   )
 }
