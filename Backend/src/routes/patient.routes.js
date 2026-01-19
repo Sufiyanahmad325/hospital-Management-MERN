481307d6fe06f84fa048d00e62b61c698f14ccdc
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2026-01-19 10:16:40 +0530

    add one router

--- Backend/src/routes/patient.routes.js
+++ Backend/src/routes/patient.routes.js
@@ -1,18 +1,19 @@
 import express from "express";
-import {
-  getMyProfile,
-  updateMyProfile,
-} from "../controllers/patient.controller.js";
+
 
 import { verifyJWT } from "../middlewares/verifyJWT.middleware.js";
 
 import { isPatient } from "../middlewares/role.middleware.js";
+import { getMyProfile, updateMyProfile } from "../controllers/Patient.controller.js";
 
 const router = express.Router();
 
 router.use(verifyJWT, isPatient);
 
-router.get("/getMyProfile", getMyProfile);
+
+
+
+router.get("/getMyProfile", getMyProfile)
 router.put("/updateMyProfile", updateMyProfile);
 
 export default router;
