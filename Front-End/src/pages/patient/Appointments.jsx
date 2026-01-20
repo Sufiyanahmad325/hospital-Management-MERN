dfd0d29d138a51075ce96cda0f5d81b5d4d418eb
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2026-01-20 09:21:48 +0530

    appointment page connected with backend

--- Front-End/src/pages/patient/Appointments.jsx
+++ Front-End/src/pages/patient/Appointments.jsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { cancelAppointment, getAllCancelledAppointment, getUpComingAppointment } from "../../reduxtollkit/patientControlSlice";
 
 const Appointments = () => {
+
+  const [category, setCategory] = useState('upcoming')
+  const [filterData, setFilterData] = useState([])
+
+  const { upComingAppointment, cancelledAppointment, completedAppointment } = useSelector((state) => state.patientControl)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (category == "upcoming" && upComingAppointment.length !== 0) {
+      setFilterData(upComingAppointment)
+    }
+    if (category == 'cancelled' && cancelledAppointment.length !== 0) {
+      setFilterData(cancelledAppointment)
+    }
+    if (category == 'completed' && completedAppointment.length !== 0) {
+      setFilterData(completedAppointment)
+    }
+  }, [upComingAppointment, cancelledAppointment, completedAppointment, category])
+
+
+  const handleCancelledAppointment =async(id)=>{
+    console.log(id)
+     let res = await dispatch(cancelAppointment(id)).unwrap()
+     console.log("kay sir =====> ",res)
+     if(res.success){
+        dispatch(getUpComingAppointment())
+        dispatch(getAllCancelledAppointment())
+     }
+  }
+
+
   return (
-    <div className="min-h-screen bg-blue-100 p-6 sm:w-[75vw] space-y-6">
+    <div className="h-screen bg-blue-100 p-6 sm:w-[75vw] space-y-6 sm:overflow-y-auto">
 
       {/* PAGE TITLE */}
       <div>
@@ -16,79 +49,66 @@ const Appointments = () => {
 
       {/* ================= FILTER BUTTONS ================= */}
       <div className="flex gap-4">
-        <button className="bg-blue-500 text-white px-4 py-2 rounded">
+        <button
+          onClick={() => setCategory('upcoming')}
+          className={`${category === 'upcoming' ? 'bg-blue-500 text-white ' : 'text-black bg-white'} border px-4 py-2 rounded`}>
           Upcoming
         </button>
 
-        <button className="bg-white border px-4 py-2 rounded">
+        <button
+          onClick={() => setCategory('completed')}
+          className={`${category === 'completed' ? 'bg-blue-500 text-white ' : 'text-black bg-white'} border px-4 py-2 rounded`}>
           Completed
         </button>
 
-        <button className="bg-white border px-4 py-2 rounded">
+        <button
+          onClick={() => setCategory('cancelled')}
+          className={`${category === 'cancelled' ? 'bg-blue-500 text-white ' : 'text-black bg-white'} border  px-4 py-2 rounded`}>
           Cancelled
         </button>
       </div>
 
       {/* ================= APPOINTMENT CARDS ================= */}
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ">
 
         {/* CARD 1 */}
-        <div className="bg-white rounded-xl shadow p-5 space-y-3">
-          <div className="flex justify-between">
-            <span className="text-xs bg-blue-100 text-blue-700 px-3 py-1 rounded-full font-semibold">
-              Upcoming
-            </span>
-            <span className="text-sm text-gray-500">
-              31 Dec 2025
-            </span>
-          </div>
+        {
+          filterData?.map((ele) => (
+            <div key={ele?._id} className="bg-white rounded-xl shadow p-5 space-y-3">
+              <div className="flex justify-between">
+                <span className="text-xs bg-blue-100 text-blue-700 px-3 py-1 rounded-full font-semibold">
+                  Upcoming {ele._id}
+                </span>
+                <span className="text-sm text-gray-500">
+                  {ele?.date}
+                </span>
+              </div>
 
-          <h3 className="text-lg font-bold text-gray-800">
-            Dr. Rahul Sharma
-          </h3>
+              <h3 className="text-lg font-bold text-gray-800">
+                {ele.doctorId.user_id.name}
+              </h3>
 
-          <p className="text-sm text-gray-500">
-            Cardiology Department
-          </p>
+              <p className="text-sm text-gray-500">
+                {ele?.doctorId?.specialization}
+              </p>
 
-          <div className="flex justify-between text-sm text-gray-600">
-            <span>⏰ 10:30 AM</span>
-            <span>📍 Room 203</span>
-          </div>
+              <div className="flex justify-between text-sm text-gray-600">
+                <span>⏰ {ele.timeSlot}</span>
+                <span>📍 Room 203</span>
+              </div>
 
-          <button className="w-full bg-red-500 hover:bg-red-600 text-white py-2 rounded">
-            Cancel Appointment
-          </button>
-        </div>
+              {
+                ele.status === "pending" && (
+                  <button onClick={()=>handleCancelledAppointment(ele._id)} className="w-full bg-red-500 hover:bg-red-600 text-white py-2 rounded">
+                Cancel Appointment
+              </button>
+                )
+              }
+            </div>
+          ))
+        }
 
-        {/* CARD 2 */}
-        <div className="bg-white rounded-xl shadow p-5 space-y-3">
-          <div className="flex justify-between">
-            <span className="text-xs bg-green-100 text-green-700 px-3 py-1 rounded-full font-semibold">
-              Completed
-            </span>
-            <span className="text-sm text-gray-500">
-              20 Dec 2025
-            </span>
-          </div>
 
-          <h3 className="text-lg font-bold text-gray-800">
-            Dr. Neha Singh
-          </h3>
-
-          <p className="text-sm text-gray-500">
-            Neurology Department
-          </p>
-
-          <div className="flex justify-between text-sm text-gray-600">
-            <span>⏰ 11:00 AM</span>
-            <span>📍 Room 101</span>
-          </div>
-
-          <button className="w-full bg-blue-500 hover:bg-blue-600 text-white py-2 rounded">
-            View Details
-          </button>
-        </div>
 
       </div>
 
