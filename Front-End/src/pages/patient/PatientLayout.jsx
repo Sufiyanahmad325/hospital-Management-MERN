fe2fd47a77ce99aa9efa31a7e94b2af8fb7ebd68
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2026-01-21 11:33:35 +0530

    API being called is here

--- Front-End/src/pages/patient/PatientLayout.jsx
+++ Front-End/src/pages/patient/PatientLayout.jsx
@@ -1,8 +1,18 @@
   import React from 'react'
 import PatientSidebar from './PatientSidebar'
 import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+import { getAllCancelledAppointment, getCompletedAppointment, getMyProfile, getUpComingAppointment } from '../../reduxtollkit/patientControlSlice'
+import { useDispatch } from 'react-redux'
 
 function PatientLayout() {
+  const dispatch = useDispatch()
+  useEffect(()=>{
+    dispatch(getMyProfile())
+    dispatch(getUpComingAppointment())
+    dispatch(getCompletedAppointment())
+    dispatch(getAllCancelledAppointment())
+  },[])
   return (
     <div className='flex flex-col sm:flex-row'>
       <PatientSidebar/>
