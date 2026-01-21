50f9cca2e9cb97b6077b5956772863d713e52585
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2026-01-21 11:31:21 +0530

    logoutpatient route has been added

--- Backend/src/routes/patient.routes.js
+++ Backend/src/routes/patient.routes.js
@@ -4,7 +4,7 @@ import express from "express";
 import { verifyJWT } from "../middlewares/verifyJWT.middleware.js";
 
 import { isPatient } from "../middlewares/role.middleware.js";
-import { getMyProfile, updateMyProfile } from "../controllers/Patient.controller.js";
+import { getMyProfile, logoutPatient, updateMyProfile } from "../controllers/Patient.controller.js";
 
 const router = express.Router();
 
@@ -15,5 +15,6 @@ router.use(verifyJWT, isPatient);
 
 router.get("/getMyProfile", getMyProfile)
 router.put("/updateMyProfile", updateMyProfile);
+router.get("/logoutPatient", logoutPatient);
 
 export default router;
