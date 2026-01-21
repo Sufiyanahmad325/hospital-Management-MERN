348f00236d8cd44290b206e54cf72e3b1a097e7b
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2026-01-21 11:52:49 +0530

    logout feature added here

--- Front-End/src/pages/patient/PatientSidebar.jsx
+++ Front-End/src/pages/patient/PatientSidebar.jsx
@@ -1,13 +1,29 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { MdDashboard } from "react-icons/md";
 import { FaCalendarCheck } from "react-icons/fa";
 import { IoPerson } from "react-icons/io5";
 import { BiLogOut } from "react-icons/bi";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { useState } from "react";
+import { useCookies } from "react-cookie";
+import { useDispatch } from "react-redux";
+import { logoutPatient } from "../../reduxtollkit/patientControlSlice";
 
 const PatientSidebar = () => {
   const [open, setOpen] = useState(false);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch()
+  const [cookies, setCookie, removeCookie] = useCookies(["accessToken"]);
+
+  const handleLogout = async () => {
+    let res = await dispatch(logoutPatient()).unwrap()
+    if (res.success) {
+      alert(res.message)
+    }
+    localStorage.removeItem("role");
+    navigate("/");
+  };
 
   const linkStyle = ({ isActive }) =>
     isActive
@@ -16,12 +32,9 @@ const PatientSidebar = () => {
 
   return (
     <div className="w-full sm:w-[25vw] sm:h-screen bg-blue-300 p-4">
-
       {/* HAMBURGER (MOBILE) */}
       <div className="sm:hidden flex justify-between">
-      <h2 className="text-xl font-bold text-blue-700 mb-1">
-        Patient Panel
-      </h2>
+        <h2 className="text-xl font-bold text-blue-700 mb-1">Patient Panel</h2>
         <GiHamburgerMenu
           size={26}
           className="text-blue-600 cursor-pointer"
@@ -30,7 +43,6 @@ const PatientSidebar = () => {
       </div>
 
       {/* TITLE */}
-
       <h2 className="hidden sm:block text-xl font-bold text-blue-700 mb-1">
         Patient Panel
       </h2>
@@ -56,10 +68,13 @@ const PatientSidebar = () => {
           My Profile
         </NavLink>
 
-        <NavLink to="/logout" className={linkStyle}>
+        <button
+          onClick={handleLogout}
+          className="flex w-full rounded-md items-center gap-3 p-2 hover:bg-blue-100"
+        >
           <BiLogOut size={20} />
           Logout
-        </NavLink>
+        </button>
       </nav>
     </div>
   );
