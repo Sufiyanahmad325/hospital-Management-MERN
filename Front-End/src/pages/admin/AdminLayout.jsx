22dde6f3e384ea7e6c1cd0e32c2a7257d29b4a49
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2026-01-22 12:20:05 +0530

    fixed some bugs

--- Front-End/src/pages/admin/AdminLayout.jsx
+++ Front-End/src/pages/admin/AdminLayout.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 import Sidebar from './Sidebar'
+import { getAllDepartments, getAllDoctorAppointments, getAllDoctors, getAllPatients, todayAllAppointments } from '../../reduxtollkit/hospitalManagementSlice';
+import { useDispatch } from 'react-redux';
 
 function adminLayout() {
+  const dispatch = useDispatch()
+ useEffect(() => {
+      // Fetch dashboard data here
+      dispatch(getAllDoctors());
+      dispatch(todayAllAppointments());
+      dispatch(getAllDoctorAppointments());
+      dispatch(getAllPatients());
+      dispatch(getAllDepartments());
+  }, [dispatch]);
+
   return (
     <div className='flex flex-col sm:flex-row sm:h-max-[100vh]'>
       <Sidebar />
