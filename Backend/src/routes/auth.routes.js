a2ae542544d9a24df0134e117787819606c9b5b3
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2026-01-22 12:19:34 +0530

    add route

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
-import {registerPatient,login} from "../controllers/authController/auth.controller.js";
+import {registerPatient,login, getMe} from "../controllers/authController/auth.controller.js";
+import { verifyJWT } from "../middlewares/verifyJWT.middleware.js";
 
 const router = express.Router();
 
@@ -7,5 +8,6 @@ const router = express.Router();
 
 router.post("/registerPatient", registerPatient); // patient register
 router.post("/login", login);               // all roles login
+router.get("/getMe",verifyJWT, getMe);               // all roles login
 
 export default router;
