b5aa80e9a8500e198c487cf07abc3aa4bcffe8c2
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2026-01-23 10:38:19 +0530

    fixed some bugs

--- Front-End/src/routes/ProtectedRoute.jsx
+++ Front-End/src/routes/ProtectedRoute.jsx
@@ -1,20 +1,66 @@
 // src/routes/ProtectedRoute.jsx
-import { useCookies } from "react-cookie";
+// import { useCookies } from "react-cookie";
+// import { Navigate } from "react-router-dom";
+// import Cookies from 'js-cookie'
+
+// const ProtectedRoute = ({ children, role }) => {
+//   // const token = localStorage.getItem("token");
+//   const [cookies, setCookie, removeCookie] = useCookies(['accessToken']);
+
+//   // const token = Cookies.get('accessToken')
+//   const token = cookies.accessToken;
+//   const userRole = localStorage.getItem("role"); // admin / doctor / patient
+
+//   if (!token) return <Navigate to="/" />;
+
+//   // yaha kya logic hai ? answer in hindi => agar role defined hai aur userRole jo localStorage se mila hai wo role ke barabar nahi hai to navigate kar do login page pe
+
+
+//   if (role && userRole !== role) return <Navigate to="/" />;
+
+//   return children;
+// };
+
+// export default ProtectedRoute;
+
+
+import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
+import axios from "axios";
 
 const ProtectedRoute = ({ children, role }) => {
-  // const token = localStorage.getItem("token");
-  const [cookies, setCookie, removeCookie] = useCookies(['accessToken']);
+  const [loading, setLoading] = useState(true);
+  const [userRole, setUserRole] = useState(null);
 
-  const token = cookies.accessToken;
-  const userRole = localStorage.getItem("role"); // admin / doctor / patient
+  useEffect(() => {
+    const checkMe = async () => {
+      try {
+        const res = await axios.get("http://localhost:8000/hospital/auth/getMe", {
+          withCredentials: true,
+        });
 
-  if (!token) return <Navigate to="/" />;
+        setUserRole(res.data.data.role);
+      } catch (err) {
+        setUserRole(null);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  // yaha kya logic hai ? answer in hindi => agar role defined hai aur userRole jo localStorage se mila hai wo role ke barabar nahi hai to navigate kar do login page pe
+    checkMe();
+  }, []);
 
-  
-  if (role && userRole !== role) return <Navigate to="/" />;
+  if (loading) {
+    return (
+      <div className="flex h-screen justify-center items-center">
+        <div className="h-12 w-12 animate-spin rounded-full border-4 border-gray-300 border-t-blue-600" />
+      </div>
+    );
+  }
+
+  if (!userRole) return <Navigate to="/" replace />;
+
+  if (role && userRole !== role) return <Navigate to="/" replace />;
 
   return children;
 };
