3cfc3cfc7d8b06864258d765039ba5be24efc250
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2026-01-26 12:04:19 +0530

    ""

--- Backend/src/utils/apiError.js
+++ Backend/src/utils/apiError.js
@@ -1,19 +1,34 @@
+// class ApiError extends Error {
+//     constructor(message = 'Something went wrong', statusCode = 500, error = [], stack = '') {
+//         // अगर message object है तो JSON बना दो
+//         super(typeof message === "object" ? JSON.stringify(message) : message);
+
+//         this.statusCode = statusCode || 500;
+//         this.error = error;
+//         this.data = null;
+//         this.success = false;
+
+//         if (stack) {
+//             this.stack = stack;
+//         } else {
+//             Error.captureStackTrace(this, this.constructor);
+//         }
+//     }
+// }
+
+// export default ApiError;
+
+
 class ApiError extends Error {
-    constructor(message = 'Something went wrong', statusCode = 500, error = [], stack = '') {
-        // अगर message object है तो JSON बना दो
-        super(typeof message === "object" ? JSON.stringify(message) : message);
+  constructor(statusCode = 500, message = "Something went wrong", errors = []) {
+    super(message);
 
-        this.statusCode = statusCode || 500;
-        this.error = error;
-        this.data = null;
-        this.success = false;
+    this.statusCode = statusCode;
+    this.errors = errors;
+    this.success = false;
 
-        if (stack) {
-            this.stack = stack;
-        } else {
-            Error.captureStackTrace(this, this.constructor);
-        }
-    }
+    Error.captureStackTrace(this, this.constructor);
+  }
 }
 
 export default ApiError;
