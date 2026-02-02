dbd0b1166805736adbd8d49c512075a470afc25e
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2026-02-02 11:03:40 +0530

    Golobal Loader has been created

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter } from 'react-router-dom';
-import AppRoutes from './routes/AppRouter';
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes/AppRouter";
+import "./App.css";
+import { useSelector } from "react-redux";
 
 function App() {
+  const isLoading = useSelector((state) =>
+    state.hospitalManagement.isLoading ||
+    state.doctorControl.isLoading ||
+    state.patientControl.isLoading
+  );
+
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+
+      {/* Global loader */}
+      {isLoading && (
+        <div className="fixed inset-0 flex flex-col justify-center items-center bg-white/60 z-50">
+          <div className="w-10 h-10 sm:w-15 sm:h-15 rounded-full border-4 border-gray-300 border-t-gray-600 animate-spin"></div>
+          <span>Loading...</span>
+        </div>
+      )}
+    </>
   );
 }
 
